b8127641fc0beb51045a7af9ea99a7d80466d6a6
Scott Ford <sford@chef.io>
2018-09-25 20:31:05 -0700

    adding vpc, subnet, gateway, route

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -9,6 +9,46 @@ resource "random_id" "instance_id" {
 }
 
 ////////////////////////////////
+// VPC 
+
+resource "aws_vpc" "national_parks_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags {
+    Name          = "${var.tag_name}-vpc"
+    X-Dept        = "${var.tag_dept}"
+    X-Customer    = "${var.tag_customer}"
+    X-Project     = "${var.tag_project}"
+    X-Contact     = "${var.tag_contact}"
+    X-Application = "${var.tag_application}"
+    X-TTL         = "${var.tag_ttl}"
+  }
+}
+
+resource "aws_internet_gateway" "national_parks_gateway" {
+  vpc_id = "${aws_vpc.national_parks_vpc.id}"
+
+  tags {
+    Name = "${var.tag_name}_national_parks_gateway"
+  }
+}
+
+resource "aws_route" "national_parks_internet_access" {
+  route_table_id         = "${aws_vpc.national_parks_vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.national_parks_gateway.id}"
+}
+
+resource "aws_subnet" "national_parks_subnet" {
+  vpc_id                  = "${aws_vpc.national_parks_vpc.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "${var.tag_name}_national_parks_subnet"
+  }
+}
+////////////////////////////////
 // Instance Data
 
 data "aws_ami" "centos" {
