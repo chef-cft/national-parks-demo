b22e3fe7f470eb7b8a973ffb8d83ee2a46193d2e
Matt Ray <matthewhray@gmail.com>
2019-04-10 16:42:29 +0900

    red dots!

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
 pkg_origin=scottford
-pkg_version=7.0.4
+pkg_version=7.0.5
 pkg_maintainer="Scott Ford <sford@chef.io>"
 pkg_license=('Apache-2.0')
 pkg_deps=(core/tomcat8 core/jre8 core/mongo-tools)
