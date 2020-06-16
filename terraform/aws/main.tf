d59b729d9035da89e647a19bed81cbc696d34841
Gregory Henkhaus <ghenkhaus@chef.io>
2020-06-16 16:20:37 -0700

    backend

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,18 +1,18 @@
 terraform {
   required_version = ">= 0.12"
   backend "s3" {
-    encrypt = true
-    bucket = "sa-s3-tf-backend"
-    region = "us-east-1"
-    key = "national-parks/terraform_state"
+    # encrypt = true
+    # bucket = "sa-s3-tf-backend"
+    # region = "us-east-1"
+    # key = "national-parks/terraform_state"
   }
 }
 
 provider "aws" {
   version = "~> 2.54"
   region                  = var.aws_region
-  profile                 = var.aws_profile
-  shared_credentials_file = "~/.aws/credentials"
+  # profile                 = var.aws_profile
+  # shared_credentials_file = "~/.aws/credentials"
 }
 
 resource "random_id" "instance_id" {
