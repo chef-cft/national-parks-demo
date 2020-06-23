4f4a5615a3ce3fdb1ce7e241dbb6d884099e0e9f
Gregory Henkhaus <ghenkhaus@chef.io>
2020-06-23 13:28:45 -0700

    added region

--- terraform/chef-automate/aws/main.tf
+++ terraform/chef-automate/aws/main.tf
@@ -3,14 +3,14 @@ terraform {
   backend "s3" {
     # encrypt = true
     # bucket = "sa-s3-tf-backend"
-    # region = "us-east-1"
+    region = "us-east-1"
     # key = "demo-validator/automate/terraform_state"
   }
 }
 
 provider "aws" {
   version = "~> 2.54"
-  # region                  = "us-east-1"
+  region                  = var.aws_region
   # profile                 = "solutions-architects"
   # shared_credentials_file = "/Users/gregoryhenkhaus/.aws/credentials"
 }
