a099ab91f5f28614d077e8faadd1a02c81e00caf
Gregory Henkhaus <ghenkhaus@chef.io>
2020-06-30 14:25:58 -0700

    test curl

--- pipeline/inspec-tests/webserver/controls/basic.rb
+++ pipeline/inspec-tests/webserver/controls/basic.rb
@@ -11,8 +11,49 @@ title "National Parks Web Server"
 control "port-1.0" do                        # A unique ID for this control
   impact 1.0                               # The criticality, if this control fails.
   title "Webserver Ports Open"             # A human-readable title
-  desc "An optional description..."
+  desc "ports exposed for webserver"
   describe port(8080) do                  # The actual test
     it { should be_listening }
   end
+end
+
+control "hab-1.0" do                        # A unique ID for this control
+  impact 1.0                               # The criticality, if this control fails.
+  title "Habitat Installed"             # A human-readable title
+  desc "habitat installed"
+  describe package('habitat') do                  # The actual test
+    it { should be_installed }
+  end
+end
+
+control "html-1.0" do                        # A unique ID for this control
+  impact 1.0                               # The criticality, if this control fails.
+  title "HTML Red Pins"             # A human-readable title
+  desc "Web page contains red pins"
+  describe package('habitat') do                  # The actual test
+    it { should be_installed }
+  end
+end
+
+
+control "audit-baseline-1.0" do                        # A unique ID for this control
+  impact 1.0                               # The criticality, if this control fails.
+  title "audit baseline exists"             # A human-readable title
+  desc "audit baseline exists"
+  describe habitat_package(origin: 'effortless', name: 'audit-baseline') do
+    it             { should exist }
+  end
+end
+
+control "config-baseline-1.0" do                        # A unique ID for this control
+  impact 1.0                               # The criticality, if this control fails.
+  title "config baseline exists"             # A human-readable title
+  desc "config baseline exists"
+  describe habitat_package(origin: 'effortless', name: 'config-baseline') do
+    it             { should exist }
+  end
+end
+
+describe command('curl http://localhost:8080/national-parks/') do
+  its(‘stdout’) { should match (/redicon.png/) }
 end
