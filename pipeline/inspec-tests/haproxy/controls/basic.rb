0f838a812158470a3d99d4d7cdc968aba3b2925d
Gregory Henkhaus <ghenkhaus@chef.io>
2020-06-30 21:05:26 -0700

    added HAProxy tests init

---
+++ pipeline/inspec-tests/haproxy/controls/basic.rb
@@ -0,0 +1,62 @@
+# copyright: 2018, The Authors
+
+title "National Parks HAProxy"
+
+# you can also use plain tests
+# describe file("/tmp") do
+#   it { should be_directory }
+# end
+
+# you add controls here
+control "port-1.0" do                        # A unique ID for this control
+  impact 1.0                               # The criticality, if this control fails.
+  title "HAProxy Ports Open"             # A human-readable title
+  desc "ports exposed for webserver"
+  describe port(8085) do                  # The actual test
+    it { should be_listening }
+  end
+end
+
+control "hab-1.0" do                        # A unique ID for this control
+  impact 1.0                               # The criticality, if this control fails.
+  title "Habitat Installed"             # A human-readable title
+  desc "habitat installed"
+  describe command('/bin/hab').exist? do
+    it { should eq true }
+  end
+end
+
+control "webpage-1.0" do                        # A unique ID for this control
+  impact 1.0                               # The criticality, if this control fails.
+  title "Web page contains content"             # A human-readable title
+  desc "Web page contains content"
+  describe command('curl http://localhost:8085/national-parks/') do
+    its('stdout') { should match (/Map of National Parks/) }
+    its('stdout') { should_not match (/redicon.png/) }
+  end
+end
+
+
+# control "webpage-1.0" do                        # A unique ID for this control
+#   impact 1.0                               # The criticality, if this control fails.
+#   title "Web page contains content"             # A human-readable title
+#   desc "Web page contains content"
+#   describe command('curl http://localhost:8080/national-parks/') do
+#     its('stdout') { should match (/redicon.png/) }
+#   end
+# end
+
+# describe habitat_services do
+#   its('names') { should include 'config-baseline' }
+#   its('names') { should include 'audit-baseline' }
+#   its('names') { should include 'national-parks' }
+# end
+
+# control "national-parks-service-1.0" do                       
+#   impact 1.0                               
+#   title "National Parks is running"   
+#   desc "config baseline exists"
+#   describe habitat_service(origin: 'chef-sa-pipeline', name: 'national-parks') do
+#     it { should exist }
+#   end
+# end
