c03e18b3d010f7408f634f1db0eb94be080476e9
Gregory Henkhaus <ghenkhaus@chef.io>
2020-07-16 10:38:51 -0700

    inspec test for a2 apis

--- pipeline/inspec-tests/automate/controls/basic.rb
+++ pipeline/inspec-tests/automate/controls/basic.rb
@@ -72,3 +72,53 @@ control "token-2.0" do
     its('status') { should eq 200 }
   end
 end
+
+
+# Perform some basic requests against the backend services
+{
+  'authn-service': {
+    path: '/apis/iam/v2/tokens',
+  },
+  'authz-service': {
+    path: '/apis/iam/v2/policy_version',
+  },
+  'config-mgmt-service': {
+    path: '/api/v0/cfgmgmt/version',
+  },
+  'compliance-service': {
+    path: '/api/v0/compliance/reporting/version',
+  },
+  'ingest-service': {
+    path: '/api/v0/ingest/version',
+  },
+  'local-user-service': {
+    path: '/apis/iam/v2/users',
+  },
+  'notifications-service': {
+    path: '/api/v0/notifications/version',
+  },
+  'teams-service': {
+    path: '/apis/iam/v2/teams',
+  },
+  'automate-gateway': {
+    path: '/api/v0/gateway/version',
+  },
+}.each_with_index do |(service_name, opts), index|
+  control "api-test-#{index}.0" do       
+    impact 1.0               
+    title "GET #{opts[:path]}"
+    desc "Checks the version endpoint of #{service_name} to make sure it's up and running"
+  
+    a2_admin_token = input('a2_admin_token')
+    a2_url = input('a2_url')
+    describe http("#{a2_url}#{opts[:path]}", 
+      method: 'GET',
+      headers: { 'api-token' => a2_admin_token }, 
+      open_timeout: 60, 
+      read_timeout: 60, 
+      ssl_verify: true, 
+      max_redirects: 5 ) do
+      its('status') { should eq(opts[:expected_status] || 200) }
+    end
+  end  
+end
