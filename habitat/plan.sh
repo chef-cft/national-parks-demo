75949262393b94d0fc50e7d4f1bcdf5f5483f86f
Dan Flores <danf425@gmail.com>
2020-07-22 16:25:12 -0400

    pin corretto to right version

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,8 +1,8 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
-pkg_origin=scottford	
+pkg_origin=scottford
 pkg_version=7.0.0
-pkg_maintainer="Scott Ford <sford@chef.io>"	
+pkg_maintainer="Scott Ford <sford@chef.io>"
 pkg_license=('Apache-2.0')
 pkg_deps=(core/tomcat8 core/corretto/11.0.2.9.3 core/mongo-tools)
 pkg_build_deps=(core/corretto/11.0.2.9.3  core/maven)
