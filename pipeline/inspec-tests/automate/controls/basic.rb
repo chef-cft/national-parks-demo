2fde2d909804a3b401166dfbcdfc8c2e6ac46370
Gregory Henkhaus <ghenkhaus@chef.io>
2020-08-06 08:57:17 -0700

    adding sleep to inspec test

--- pipeline/inspec-tests/automate/controls/basic.rb
+++ pipeline/inspec-tests/automate/controls/basic.rb
@@ -1,6 +1,8 @@
 # copyright: 2018, The Authors
 
 title "Automate"
+a2_admin_token = input('a2_admin_token')
+a2_url = input('a2_url')
 
 control "port-1.0" do               
   impact 1.0                        
@@ -35,11 +37,11 @@ control "hab-1.0" do
   end
 end
 
+# Testing
+sleep(10)
+
 # Perform some basic requests against the backend services
 {
-  'authn-service': {
-    path: '/apis/iam/v2/tokens',
-  },
   'authn-service': {
     path: '/apis/iam/v2/tokens',
   },
@@ -72,9 +74,7 @@ end
     impact 1.0               
     title "GET #{opts[:path]}"
     desc "Checks the version endpoint of #{service_name} to make sure it's up and running"
-  
-    a2_admin_token = input('a2_admin_token')
-    a2_url = input('a2_url')
+
     describe http("#{a2_url}#{opts[:path]}", 
       method: 'GET',
       headers: { 'api-token' => a2_admin_token }, 
